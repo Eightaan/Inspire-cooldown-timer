f65a06b5fbea93147ed9a067fde29c949128a3e4
Eightaan <57832730+Eightaan@users.noreply.github.com>
2024-10-07 23:28:26 +0200

    Add files via upload

--- Inspire Cooldown Timer/lua/InspireCooldownTimer.lua
+++ Inspire Cooldown Timer/lua/InspireCooldownTimer.lua
@@ -1,86 +1,135 @@
-local Color = Color
 if RequiredScript == "lib/managers/hudmanagerpd2" then
+	Hooks:PostHook(HUDManager, "_setup_player_info_hud_pd2", "InspireCT_bufflist_setup_player_info_hud_pd2", function(self, ...)
+        self._hud_buff_list = HUDBuffList:new(managers.hud:script(PlayerBase.PLAYER_INFO_HUD_PD2))
+    end)
 
-	Hooks:PostHook(HUDManager, "_player_hud_layout", "EIVH_inspire_cooldown_timer", function(self)
-		InspireCT:init()
-	end)
-
-	function InspireCT:init()
+    function HUDManager:update_inspire_timer(buff)
+        self._hud_buff_list:update_inspire_timer(buff)
+    end
+	
+	HUDBuffList = HUDBuffList or class()
+	function HUDBuffList:init()
 		if managers.hud ~= nil then 
-			self.hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_FULLSCREEN_PD2)
-			self._cooldown_panel = self.hud.panel:panel({
-            name = "cooldown_panel",
-            x = 0,
-            y = 0
-        })
-			self.cooldown_text = self._cooldown_panel:text({
-				layer = 2,
-				visible = false,
-				text = "0.0",
-				font = tweak_data.hud.medium_font_noshadow,
-				font_size = 20,
-				x = 14,
-				y = 30,
-				color = Color.white
-			})
-			self._inspire_cooldown_icon = self._cooldown_panel:bitmap({
-				name = "inspire_cooldown_icon",
-				texture = "guis/textures/pd2/skilltree_2/icons_atlas_2",
-				texture_rect = { 4* 80, 9 * 80, 80, 80 },
-				w = 38,
-				h = 38,
-				x = 0,
-				y = 0,
-				color = Color.white,
-				visible = false,
-				layer = 1
-			})
-			self._inspire_cooldown_timer_bg = self._cooldown_panel:bitmap({
-				name = "inspire_cooldown_timer_bg",
-				texture = "guis/textures/pd2/crimenet_marker_glow",
-				w = 42,
-				h = 42,
-				x = 0,
-				y = 20,
-				color = Color("66ffff"),
-				alpha = 0.5,
-				visible = false,
-				layer = 0
+		    self.hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_FULLSCREEN_PD2)
+            self._cooldown_panel = self.hud.panel:panel({
+                name = "cooldown_panel",
+                x = 0,
+                y = 0
+            })
+            self.cooldown_text = self._cooldown_panel:text({
+                layer = 2,
+                visible = false,
+                text = "",
+                font = tweak_data.hud.medium_font_noshadow,
+                font_size = 16,
+                x = 13,
+                y = 25,
+                color = Color.white
+            })
+            self._inspire_cooldown_icon = self._cooldown_panel:bitmap({
+                name = "inspire_cooldown_icon",
+                texture = "guis/textures/pd2/skilltree_2/icons_atlas_2",
+                texture_rect = { 4 * 80, 9 * 80, 80, 80 },
+                w = 28,
+                h = 28,
+                x = 0,
+                y = 0,
+                color = Color.white,
+                visible = false,
+                layer = 1
+            })
+            self._inspire_cooldown_timer_bg = self._cooldown_panel:bitmap({
+                name = "inspire_cooldown_timer_bg",
+                texture = "guis/textures/pd2/crimenet_marker_glow",
+                texture_rect = { 1, 1, 62, 62 }, 
+                w = 40,
+                h = 40,
+                x = 0,
+                y = 13,
+                color = Color("66ffff"),
+                visible = false,
+                layer = 0
 			})
 		end
 	end
+	
+	function HUDBuffList:update_timer_visibility_and_position()
+        local inspire_visible = InspireCT.Options:GetValue("Inspire")
+        self.cooldown_text:set_visible(inspire_visible)
+        self._inspire_cooldown_timer_bg:set_visible(inspire_visible)
+        self._inspire_cooldown_icon:set_visible(inspire_visible)
 
-	function InspireCT:update_inspire_timer(t)
-		local timer = self.cooldown_text
-		local panel = self._cooldown_panel
-		local timer_bg = self._inspire_cooldown_timer_bg
-		local icon = self._inspire_cooldown_icon
-    	if t and t > 1 and timer then
-        	timer:stop()
-        	timer:animate(function(o)
-            	o:set_visible(true)
-				timer_bg:set_visible(true)
-				icon:set_visible(true)
-				panel:set_x(10 * InspireCT.Options:GetValue("TimerX"))
-				panel:set_y(10 * InspireCT.Options:GetValue("TimerY"))
-            	local t_left = t
-            	while t_left >= 0.1 do
-                	t_left = t_left - coroutine.yield()
-					t_format = t_left < 10 and "%.1f" or "%.f"
-                	o:set_text(string.format(t_format, t_left))
-            	end
-				icon:set_visible(false)
-				timer_bg:set_visible(false)
-            	o:set_text(false)
-        	end)
-    	end
+        local pos_x = 10 * (InspireCT.Options:GetValue("TimerX") or 0)
+        local pos_y = 10 * (InspireCT.Options:GetValue("TimerY") or 0)
+        self._cooldown_panel:set_x(pos_x)
+        self._cooldown_panel:set_y(pos_y)
+    end
+
+    function HUDBuffList:update_inspire_timer(duration)
+        local timer = self.cooldown_text
+        local timer_bg = self._inspire_cooldown_timer_bg
+        local icon = self._inspire_cooldown_icon
+
+        timer:set_alpha(1)
+        timer_bg:set_alpha(0.5)
+        icon:set_alpha(1)
+
+        if duration and duration > 1 then
+            timer:stop()
+            timer:animate(function(o)
+                local t_left = duration
+				self:update_timer_visibility_and_position()
+
+                while t_left >= 0 do
+                    if t_left <= 0.1 then
+                        self:fade_out("inspire")
+                        return
+                    end
+                    t_left = t_left - coroutine.yield()
+                    o:set_text(string.format(t_left < 9.9 and "%.1f" or "%.f", t_left))
+                    self:update_timer_visibility_and_position()
+                end
+            end)
+        end
+    end
+	
+	function HUDBuffList:fade_out(buff_type)
+		local start_time = os.clock()
+		local text, bg, icon
+		local duration
+
+		if buff_type == "inspire" then
+			text = self.cooldown_text
+			bg = self._inspire_cooldown_timer_bg
+			icon = self._inspire_cooldown_icon
+			duration = 0.1
+		end
+
+		if text and bg and icon then
+			text:animate(function()
+				while true do
+					local alpha = math.max(1 - (os.clock() - start_time) / duration, 0)
+					text:set_alpha(alpha)
+					bg:set_alpha(0.5 * alpha)
+					icon:set_alpha(alpha)
+
+					if alpha <= 0 then
+						icon:set_visible(false)
+						bg:set_visible(false)
+						text:set_text("")
+						break
+					end
+
+					coroutine.yield()
+				end
+			end)
+		end
 	end
 elseif RequiredScript == "lib/managers/playermanager" then
-	Hooks:PreHook(PlayerManager, "disable_cooldown_upgrade", "HMH_PlayerManager_disable_cooldown_upgrade", function (self, category, upgrade)
-		local upgrade_value = self:upgrade_value(category, upgrade)
-		if upgrade_value == 0 then return end
-		if InspireCT.Options:GetValue("Inspire") then
-			InspireCT:update_inspire_timer(upgrade_value[2])
-		end
-	end)
+		Hooks:PostHook(PlayerManager, "disable_cooldown_upgrade", "InspireCT_PlayerManager_disable_cooldown_upgrade", function(self, category, upgrade)
+        local upgrade_value = self:upgrade_value(category, upgrade)
+        if upgrade_value and upgrade_value[1] ~= 0 and InspireCT.Options:GetValue("Inspire") then
+            managers.hud:update_inspire_timer(upgrade_value[2])
+        end
+    end)
 end
